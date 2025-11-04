e68b57dce4bd8d8b2e134b93cbee4b27b77f85a9
pandson7 <pandson@amazon.com>
2025-11-04 00:02:02 -0500

    Add getAllProducts Lambda function

---
+++ goat-api-cdk/lambda/getAllProducts.js
@@ -0,0 +1,36 @@
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, ScanCommand } = require('@aws-sdk/lib-dynamodb');
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+exports.handler = async (event) => {
+  try {
+    const command = new ScanCommand({
+      TableName: process.env.PRODUCTS_TABLE_NAME,
+    });
+
+    const result = await docClient.send(command);
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, X-Amz-Date, Authorization, X-Api-Key',
+      },
+      body: JSON.stringify(result.Items || []),
+    };
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ error: 'Failed to fetch products' }),
+    };
+  }
+};
